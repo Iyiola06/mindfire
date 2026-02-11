5e83c43a9dc6820fa7d86d944f4792253fcec4f3
Iyiola Ogunjobi <ogunjobiniyiola906@gmail.com>
2026-02-11 17:43:19 +0100

    working

--- app/properties/page.tsx
+++ app/properties/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import React, { useState, useEffect } from 'react';
-import Link from 'next/link';
+
 import { PublicLayout } from '@/components/layout/PublicLayout';
 import { PropertyCard } from '@/components/shared/PropertyCard';
 import { MOCK_PROPERTIES } from '@/constants';
