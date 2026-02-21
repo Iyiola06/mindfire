73836ab2a175cb5ae0d1fc9a5dbf77a629261b41
Iyiola Ogunjobi <ogunjobiniyiola906@gmail.com>
2026-02-21 14:37:41 +0100

    done

--- app/robots.ts
+++ app/robots.ts
@@ -1,6 +1,6 @@
 import { MetadataRoute } from 'next';
 
-const BASE_URL = process.env.NEXTAUTH_URL || 'https://mindfirehomes.com';
+const BASE_URL = process.env.NEXT_PUBLIC_SITE_URL || 'https://mindfirehomes.com';
 
 export default function robots(): MetadataRoute.Robots {
     return {
